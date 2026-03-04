e103e5f699fda5ff169f92a0fa94c3ce697b3ca1
Ei_chann <rifqi070409@gmail.com>
2026-03-04 22:41:05 +0700

    add logout system

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,24 @@
+import { useNavigate } from "react-router-dom";
+import api from "../services/api"
 import './Nav.css'
 import { Link } from "react-router-dom";
 
 function Nav(){
+
+    const navigate = useNavigate();
+
+    const handleLogout = async () => {
+        try {
+        await api.post("/logout");
+
+        localStorage.removeItem("token");
+
+        navigate("/login");
+        } catch (error) {
+        console.log(error);
+        }
+    };
+
     return(
         <div className='container-nav'>
             <h1>Apotek</h1>
@@ -15,7 +32,7 @@ function Nav(){
                 </ul>
                 <ul className='p'>
                     <a href=""><li>Setting</li></a>
-                    <li><button type='submit'>Logout</button></li>
+                    <li className="logout"><button onClick={handleLogout}>Logout</button></li>
                 </ul>
             </nav>
         </div>
