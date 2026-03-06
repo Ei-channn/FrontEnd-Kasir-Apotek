a906b199008540e08bccd02dd1f8e6226ff3c63f
Ei_chann <rifqi070409@gmail.com>
2026-03-06 19:07:40 +0700

    styling login page

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import api from "../../services/api";
+import "./Login.css"
 
 function Login() {
   const [email, setEmail] = useState("");
@@ -28,21 +29,57 @@ function Login() {
   };
 
   return (
-    <form onSubmit={handleLogin}>
-      <input
-        type="email"
-        placeholder="Email"
-        onChange={(e) => setEmail(e.target.value)}
-      />
+    <div className="container-login">
+      <div class="bg-decoration"></div>
+      <div class="bg-decoration"></div>
+      <div class="bg-decoration"></div>
 
-      <input
-        type="password"
-        placeholder="Password"
-        onChange={(e) => setPassword(e.target.value)}
-      />
+      <div class="login-container">
+        <div class="login-header">
+          <h1>Apotek Santi Medika</h1>
+          <p>Silakan masuk ke akun Anda</p>
+        </div>
+        <form onSubmit={handleLogin} class="login-form" id="loginForm">
+          <div class="form-group">
+            <label for="email">Email</label>
+            <div class="input-wrapper">
+              <input
+                type="email"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+          </div>
 
-      <button type="submit">Login</button>
-    </form>
+          <div class="form-group">
+            <label for="password">Password</label>
+            <div class="input-wrapper">
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+          </div>
+
+          <div class="form-options">
+            <div class="remember-me">
+              <input type="checkbox" id="remember" name="remember" />
+              <label for="remember">Ingat saya</label>
+            </div>
+          </div>
+          <button type="submit" class="btn-login" id="loginBtn">Login</button>
+        </form>
+
+        <div class="divider">
+            <span>atau</span>
+        </div>
+
+        <div class="register-link">
+            Belum punya akun? <a href="#">Register disini!!</a>
+        </div>
+      </div>
+    </div>
   );
 }
 
